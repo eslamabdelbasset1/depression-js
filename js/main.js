c77151f34c377a263afeb2996b9f9a7ce22330b3
Eslam Abdelbasset <eslamelbazedy@gmail.com>
2021-12-27 22:37:34 +0200

    init commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const videoElement1 = document.getElementById('vid1');
+const videoElement2 = document.getElementById('vid2');
+const videoElement1Con = document.getElementById('vid1Con');
+const videoElement2Con = document.getElementById('vid2Con');
+
+
 const sections = document.querySelectorAll('.section');
 let takenHugs = 0;
 let hugs = [
@@ -48,6 +54,14 @@ document.getElementById('m3lshBtn').addEventListener('click', () => {
     document.getElementById('blue').classList.add('active');
 });
 
+document.getElementById('btn2').addEventListener('click', () => {
+    sections.forEach(element => {
+        element.classList.remove('active');
+    });
+    document.getElementById('sad').classList.add('active');
+});
+
+
 let changeHug = () => {
     if(takenHugs === 5){
         document.getElementById('block_hugs').style.display = 'flex';
@@ -67,3 +81,66 @@ let changeHug = () => {
 }
 
 document.getElementById('hugAgain').addEventListener('click', changeHug);
+
+
+
+
+document.getElementById('startBtn').addEventListener('click', (event) => {
+    document.getElementById('vidsCont').style.display = 'block';
+    document.getElementById('texts').style.display = 'none';
+
+    disableScroll();
+    goToScroll(videoElement1Con.id);
+    videoElement1.play();
+    videoElement2Con.style.display = "none"
+
+    setInterval(() => {
+        if(videoElement1.currentTime > 6){
+            videoElement1.pause();
+            videoElement2Con.style.display = "flex"
+            console.log(videoElement1.currentTime)
+            goToScroll(videoElement2Con.id)
+            videoElement2.play();
+            videoElement1Con.style.display = "none"
+
+        }
+    },500)
+})
+
+
+
+let goToScroll = (elm) => {
+    location.href = `#${elm}`;
+}
+
+// call this to Disable scrolling
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
